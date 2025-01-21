a888c1b547f482b682540fdf55d2715e256b738a
useriskhai <114894656+useriskhai@users.noreply.github.com>
2025-01-21 12:35:57 +0900

    Update metrics.ts

--- supabase/functions/_shared/utils/metrics.ts
+++ supabase/functions/_shared/utils/metrics.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'npm:@supabase/supabase-js@2';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.1';
 import { ProcessMetrics } from '../types/queue.ts';
 import { Database } from '../database.types.ts';
 
